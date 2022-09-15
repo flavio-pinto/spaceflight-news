940f9f12e01a0845c278413b22d0330ea0b56481
Flavio Pinto <flaviopnt@hotmail.it>
2022-09-15 19:27:35 +0200

    [FEAT] Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        
+      <div className="App bg-dark text-light">
+        <Header />
       </div>
     </BrowserRouter>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import { Container } from "react-bootstrap";
+
+const Header = () => {
+  return(
+    <Container>
+      <h1 className="title display-1 py-5">SPACEFLIGHT<span className="d-block">✬ NEWS ✬</span></h1>
+    </Container>
+  )
+}
+
+export default Header;
